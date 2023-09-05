c192e0cb0cc0838a1651d4adc20c0e63ed8a7b0a
mortaldelombre <144105728+mortaldelombre@users.noreply.github.com>
2023-09-05 10:23:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# calculatrice
+ prijet préap
---
+++ calculatrice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bienvenue dans la calculatrice");
+            Console.WriteLine("veuillez entrer une opération");
+
+            Console.WriteLine();
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
